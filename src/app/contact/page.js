c3d6706ee489f4ab96d2f40de622ccfcef2edce6
Deepak Nagar <ddhakad745@gmail.com>
2023-11-03 14:28:24 +0530

    Linking and Navigation

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-bold text-5xl">Contact page</h1>
+    <main className="p-24">
+      <h1 className="text-bold text-5xl">Contact page</h1><br />
+      <Link href='/'>Go to Home page</Link>
     </main>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
 'use client'
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Home() {
 
+  const router = useRouter();
+  const navigate = (route) => {
+    router.push(route);
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-bold text-5xl">Home page</h1>
+    <main className=" min-h-screen p-24">
+      <h1 className="text-bold text-5xl">Home page</h1> <br />
+      <Link href={'/about'}>Go to About page</Link> <br />
+      <Link href={'/contact'}>Go to Contact page</Link> <br />
+      <br /><br /><br /><br />
+      <button onClick={()=>navigate('about')} >Go to About page</button>
+      <button onClick={()=>navigate('contact')} className="mx-16">Go to Contact page</button>
     </main>
   )
 }
