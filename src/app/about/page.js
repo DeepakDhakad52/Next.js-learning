0eb5528d07cfe1f9a644f78d03e5236c36112bb3
Deepak Nagar <ddhakad745@gmail.com>
2023-11-03 14:42:55 +0530

    'Nested Routing'

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,9 @@ const page = () => {
   return (
     <main className=" p-24">
       <h1 className="text-bold text-5xl">About page</h1><br />
-      <Link href={'/'}>Go to Home page</Link>
+      <Link href={'/'}>Go to Home page</Link><br />
+      <Link href={'/about/student'}>Go to Student Portal</Link><br />   
+      <Link href={'/about/teacher'}>Go to Teacher Portal</Link>
     </main>
   )
 }
---
+++ src/app/about/student/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="p-24">
+    <h1 className="text-bold text-5xl">About Student Portal</h1><br />
+    {/* <Link href='/'>Go to Home page</Link><br /> */}
+    <Link href='/about'>Go to About page</Link><br /> 
+  </main>
+  )
+}
+
+export default page
