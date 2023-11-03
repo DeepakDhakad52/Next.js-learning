c4f069c5a013df8862aca9a0e0dfe32322f9aada
Deepak Nagar <ddhakad745@gmail.com>
2023-11-03 14:15:28 +0530

    Basic Routing

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-bold text-5xl">About page</h1>
+    </main>
+  )
+}
+
+export default page
--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,10 @@ import React from "react";
 
 export default function Home() {
 
-  const [fullname, setFullname] = React.useState('Deepak');
-
-  const app = (item) => {
-    alert(item);
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello My name is {fullname}</h1>
-      <button onClick={()=>app('hello')}>Hello</button>
+      <h1 className="text-bold text-5xl">Home page</h1>
     </main>
   )
 }
