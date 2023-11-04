a2738fbe5052bbcd027cce9922dfa4e4d598c4ed
Deepak Nagar <ddhakad745@gmail.com>
2023-11-04 19:34:26 +0530

    Modular CSS | CSS Modules

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 'use client'
 import React from "react";
+import custom from './custom.module.css'
+import outside from './outside.module.css'
+import other from '@/styles/other.module.css'
 
 export default function Home() {
 
   return (
     <main className=" min-h-screen px-24 py-4">
       <h1 className="text-center text-bold text-5xl">Home page</h1> <br />
-      <h3 className="text-center" style={{color : 'green'}}>Leaning Next js</h3> {/* Inline css */}
+      <h3 className={custom.main} >Leaning Next js</h3>
+      <h3 className={other.main} >Leaning Next js</h3>
+      <h3 className={outside.main} >Leaning Next js</h3>
     </main>
   )
 }
