f9133e4468ba82d49a41bd989e21703d9dc09ad0
Deepak Nagar <ddhakad745@gmail.com>
2023-11-04 19:19:57 +0530

    Global CSS and Inline CSS

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className=" min-h-screen px-24 py-4">
       <h1 className="text-center text-bold text-5xl">Home page</h1> <br />
+      <h3 className="text-center" style={{color : 'green'}}>Leaning Next js</h3> {/* Inline css */}
     </main>
   )
 }
