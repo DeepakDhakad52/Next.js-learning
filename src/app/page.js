04e24dbbf3091b96a1d6666228ee9859d50d5359
Deepak Nagar <ddhakad745@gmail.com>
2023-11-05 22:18:08 +0530

    Image Component in Next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,21 @@
 'use client'
 import React from "react";
-import custom from './custom.module.css'
-import outside from './outside.module.css'
-import other from '@/styles/other.module.css'
+import Image from "next/image";
+import profile from '../../public/vercel.svg'
 
 export default function Home() {
+  // console.log(profile);
 
   return (
     <main className=" min-h-screen px-24 py-4">
       <h1 className="text-center text-bold text-5xl">Home page</h1> <br />
-      <h3 className={custom.main} >Leaning Next js</h3>
-      <h3 className={other.main} >Leaning Next js</h3>
-      <h3 className={outside.main} >Leaning Next js</h3>
+      {/* <Image src={profile} />
+      <img src={profile.src} /> */}
+      <Image
+        src={'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/2560px-Flag_of_India.svg.png'}
+        width={500}
+        height={500}
+      /> {/* This Image will not work until you will configure the domain in next.config.js file */}
     </main>
   )
 }
