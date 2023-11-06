6a471bec878400cea7d13e74152ab927ccfcd05c
Deepak Nagar <ddhakad745@gmail.com>
2023-11-06 19:15:46 +0530

    Font Family in Next JS

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      {/* <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head> */}
+
       <body className={inter.className}>
-      <Navbar />
+        <Navbar />
         {children}</body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
 'use client'
 import React from "react";
-import Image from "next/image";
-import profile from '../../public/vercel.svg'
+import { Poppins } from "next/font/google";
+const poppins =  Poppins({
+  weight: '100',
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export default function Home() {
-  // console.log(profile);
-
   return (
     <main className=" min-h-screen px-24 py-4">
       <h1 className="text-center text-bold text-5xl">Home page</h1> <br />
-      {/* <Image src={profile} />
-      <img src={profile.src} /> */}
-      <Image
-        src={'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/2560px-Flag_of_India.svg.png'}
-        width={500}
-        height={500}
-      /> {/* This Image will not work until you will configure the domain in next.config.js file */}
+      <h2 className={poppins.className}>Hello, I am Deepak!</h2>
+      {/* <h2 style={{fontFamily:'Poppins', fontWeight:100}}>Hello, I am Deepak!</h2> */}
     </main>
   )
 }
