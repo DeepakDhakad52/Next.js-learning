5ed28701b63a2d8f489cda88b8e2b5f9bdd51cfd
Deepak Nagar <ddhakad745@gmail.com>
2023-11-09 22:33:02 +0530

    Static HTML page with build in next js

---
+++ src/app/seller/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import User from '../components/User'
+
+const page = () => {
+    return (
+        <main className=" min-h-screen px-24 py-4">
+            <h1 className="text-center text-bold text-5xl">Seller Page</h1> <br />
+            <User />
+        </main>
+    )
+}
+
+export default page
