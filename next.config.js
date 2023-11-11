1cbd2236935285a8468498ca1519495719ea55d6
Deepak Nagar <ddhakad745@gmail.com>
2023-11-11 18:45:11 +0530

    Redirection in Next JS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     redirects: async () => {
         return [
             {
@@ -23,11 +22,6 @@ const nextConfig = {
 }                   
 
 // Redirection using config file
-=======
-    images : {
-        domains:['upload.wikimedia.org']
-    }
-}
->>>>>>> d12923f880099393ee370e83053059bfff1c8338
+
 
 module.exports = nextConfig
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { redirect } from "next/navigation"
 
 
@@ -7,23 +6,6 @@ const page = async () => {
     return (
         <main className=" min-h-screen px-24 py-4">
             <h1 className="text-center text-bold text-5xl">User Page</h1>
-=======
-import Link from "next/link";
-import getUser from "../../../services/getUser"
-
-const page = async () => {
-    const getUserList = getUser();
-    const users = await getUserList;
-    return (
-        <main className=" min-h-screen px-24 py-4">
-            <h1 className="text-center text-bold text-5xl">User List</h1>
-            {
-                users.map(user =>
-                    <h2 key={user.id}>
-                        <Link href={`/users/${user.id}`}>{user.name}</Link>
-                    </h2>)
-            }
->>>>>>> d12923f880099393ee370e83053059bfff1c8338
         </main>
     )
 }
