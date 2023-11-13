b867e9f9cc0400e52bbc629401c0f4318fb55344
Deepak Nagar <ddhakad745@gmail.com>
2023-11-13 14:28:24 +0530

    Create Post API

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,3 +5,19 @@ export function GET(){
     const data = user
     return NextResponse.json(data, {status:200});
 }
+
+export async function POST(req){
+    // console.log(req);
+    let payload = await req.json();
+    // console.log(payload.name);
+    if(!payload.name || !payload.age || !payload.city || !payload.email){
+        return NextResponse.json({
+            result: "Required All fields",
+            success: false
+        },{status:400})
+    }
+    return NextResponse.json({
+        result: "User Created",
+        success: true
+    }, {status:201})
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className=" min-h-screen px-24 py-4">
       <h1 className="text-center text-bold text-5xl">Home page</h1>
-      <h1 className="text-center text-bold text-5xl mt-5"> Call Next js API which create in API routes</h1>
+      <h1 className="text-center text-bold text-5xl mt-5">Creating Post API</h1>
     </main>
   )
 }
